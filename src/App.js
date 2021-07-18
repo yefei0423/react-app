740dad4d44e139db872cfc606eb23f070a9c6674
Ye Fei <afei_234@163.com>
2021-07-18 11:22:02 +0800

    update the App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+  let moreCode = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z'];
+  let codes = '';
+  for (let i = 0; i < 18; i++) {
+      let a = parseInt(Math.random() * moreCode.length);
+      codes += moreCode[a];
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -15,8 +30,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 
         </a>
+        <p>随机码：{codes}</p>
       </header>
     </div>
   );
